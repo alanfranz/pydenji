a021e6f2da3ebdc78cfd9dd41409a7122cac5efe
Alan Franzoni <username@franzoni.eu>
2010-08-09 22:27:07 +0200

    Initial checkin after code cleanup.

---
+++ src/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(name='pydenji',
+      version='0.1',
+      description='Dependency Injection Toolkit',
+      author='Alan Franzoni',
+      license='APL 2.0',
+      keywords='dependency injection inversion control ioc container',
+      author_email='username@franzoni.eu',
+      url='http://pydenji.franzoni.eu',
+      packages=find_packages()
+     )
