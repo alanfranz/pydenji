b7bf36a42dfbf00e61c4700920f09a74084230fb
Alan Franzoni <username@franzoni.eu>
2010-09-12 01:05:57 +0200

    Enh: basic property configuration.

--- pydenji/pydenji/userproperties/mapping.py
+++ pydenji/pydenji/userproperties/mapping.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 # (C) 2010 Alan Franzoni.
 
+from configobj import ConfigObj
+
 _NO_VALUE = object()
 
 def map_properties_to_obj(d, obj, map_nonexistent=False):
@@ -11,3 +13,24 @@ def map_properties_to_obj(d, obj, map_nonexistent=False):
             raise ValueError, "Object '%s' hasn't got a '%s' attribute" % (obj, key)
         setattr(obj, key, value)
     return obj
+
+class ConfigObjPropertyMapper(object):
+    def __init__(self, configobj_source):
+        self._co = ConfigObj(configobj_source, unrepr=True)
+
+    def __call__(self, config_cls):
+        # FIXME: we'll need a utility "aop" function to intercept calls.
+        original_init = config_cls.__init__
+        def new_init(new_self, *args, **kwargs):
+            original_init(new_self, *args, **kwargs)
+            # TODO: should we let the config name to be set explicitly?
+            # TODO: name is changed by our decorator right now, maybe it shouldn't.
+            map_properties_to_obj(self._co[config_cls.__name__], new_self)
+            
+        config_cls.__init__ = new_init
+        return config_cls
+
+
+
+
+
