863af8b89d206093a62942bb6694d8d413f617b0
Alan Franzoni <username@franzoni.eu>
2010-09-11 14:44:48 +0200

    Enh: support arbitrary property mapping.

--- pydenji/pydenji/userproperties/mapping.py
+++ pydenji/pydenji/userproperties/mapping.py
@@ -3,9 +3,9 @@
 
 _NO_VALUE = object()
 
-def map_properties_to_obj(d, obj):
+def map_properties_to_obj(d, obj, map_nonexistent=False):
     for key, value in d.iteritems():
-        if getattr(obj, key, _NO_VALUE) is _NO_VALUE:
+        if (getattr(obj, key, _NO_VALUE) is _NO_VALUE) and not map_nonexistent:
             raise ValueError, "Object '%s' hasn't got a '%s' attribute" % (obj, key)
         setattr(obj, key, value)
     return obj
