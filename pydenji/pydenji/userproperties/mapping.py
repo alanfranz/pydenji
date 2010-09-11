26b46243fce58c23d1ddbdfc8c1b6547575d4618
Alan Franzoni <username@franzoni.eu>
2010-09-11 14:40:42 +0200

    Fix: ValueError message formatting. Added test.

--- pydenji/pydenji/userproperties/mapping.py
+++ pydenji/pydenji/userproperties/mapping.py
@@ -6,6 +6,6 @@ _NO_VALUE = object()
 def map_properties_to_obj(d, obj):
     for key, value in d.iteritems():
         if getattr(obj, key, _NO_VALUE) is _NO_VALUE:
-            raise ValueError, "Object '%s' hasn't got a '%s' attribute" % key
+            raise ValueError, "Object '%s' hasn't got a '%s' attribute" % (obj, key)
         setattr(obj, key, value)
     return obj
