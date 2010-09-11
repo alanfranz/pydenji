4f68462f73ef0d9c69ae922413aea0bd78a9a8e0
Alan Franzoni <username@franzoni.eu>
2010-09-11 14:46:20 +0200

    Enh: add memo.

--- pydenji/pydenji/userproperties/mapping.py
+++ pydenji/pydenji/userproperties/mapping.py
@@ -5,6 +5,7 @@ _NO_VALUE = object()
 
 def map_properties_to_obj(d, obj, map_nonexistent=False):
     for key, value in d.iteritems():
+        # TODO: this is a simple check, what if "value" is a method?
         if (getattr(obj, key, _NO_VALUE) is _NO_VALUE) and not map_nonexistent:
             raise ValueError, "Object '%s' hasn't got a '%s' attribute" % (obj, key)
         setattr(obj, key, value)
