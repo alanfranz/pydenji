44ee42c1d8ef91a0af5536f85bb0a1d2c96c95f4
Alan Franzoni <username@franzoni.eu>
2010-09-11 14:31:27 +0200

    Initial mapping support code.

---
+++ pydenji/pydenji/userproperties/mapping.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+_NO_VALUE = object()
+
+def map_properties_to_obj(d, obj):
+    for key, value in d.iteritems():
+        if getattr(obj, key, _NO_VALUE) is _NO_VALUE:
+            raise ValueError, "Object '%s' hasn't got a '%s' attribute" % key
+        setattr(obj, key, value)
+    return obj
