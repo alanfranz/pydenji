c9015175b1a55525494a2675bb68ee9f051ef8da
Alan Franzoni <username@franzoni.eu>
2010-09-12 13:48:18 +0200

    Fix: forgotten test case.

---
+++ pydenji/pydenji/test/test_configobj_mapping.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# (C) 2010 Alan Franzoni.
+
+import unittest
+from pydenji.userproperties.mapping import map_properties_to_obj, ConfigObjPropertyMapper
+
+
+class  Test_configobj_mappingTestCase(unittest.TestCase):
+    example_config = """
+    # for an object
+    [object]
+    property1="a"
+    property2="b"
+
+    # when are they set? automagically? explicitly? they can be both set?
+    # probably: if property1 exists, then it is set, otherwise it isn't, and
+    # everything is available in the config as well.
+
+    # do I want to deliver something to a config itself?
+    [SomeConfig]
+    prop1="x"
+
+    # placeholders resolution? there're no java properties here...
+    # but my config REALLY needs something from outside.
+    # special placeholder and if not resolved crash after instantiation?
+
+    e.g. something like
+
+    @Configuration
+    class MyConfig(object):
+        someprop = MustBeSet()
+
+        ...
+
+    
+
+
+    """
+    class TestObj(object):
+        property1 = None
+
+
+    def test_dictmapper_maps_existent_properties(self):
+        o = self.TestObj()
+        map_properties_to_obj({"property1": 5}, o)
+        self.assertEquals(5, o.property1)
+
+
+    def test_dictmapper_forbids_mapping_non_existent_properties_by_default(self):
+        o = self.TestObj()
+        self.assertRaises(ValueError, map_properties_to_obj,
+            {"property2": 5}, o)
+
+    def test_dictmapper_allows_mapping_non_existent_properties_if_asked(self):
+        o = self.TestObj()
+        map_properties_to_obj({"property2": 5}, o, map_nonexistent=True)
+        self.assertEquals(5, o.property2)
+
+    def test_mapping_properties_to_config(self):
+        class MockConfig(object):
+            property1 = 1
+
+        print MockConfig.__name__
+
+        config = ConfigObjPropertyMapper(
+        ["[MockConfig]", "property1=123"]
+        )(MockConfig)()
+        self.assertEquals(123, config.property1)
+
+
+
+
+
