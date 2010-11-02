d385b26f71066d5ee0bd24d68b949801f5731a45
Alan Franzoni <username@franzoni.eu>
2010-11-03 00:59:22 +0100

    Enh: support read and write resources.

--- pydenji/pydenji/resourceloader.py
+++ pydenji/pydenji/resourceloader.py
@@ -9,6 +9,7 @@
 # how to work with pkg_resources split packages, e.g. those using namespace_packages
 # how will this play along distutils2 and its pkgutil.open()
 
+import os
 from urlparse import urlparse
 
 from pkg_resources import resource_filename
@@ -47,16 +48,89 @@ supported_schemes = {
     "pkg": package_uri_resolver,
 }
 
-
 # should it break if resource does not exist?
 # maybe optionally? a differenct factory? readresourceloader, writeresourceloader,
 # mayberesourceloader?
+# check: filepath is always absolute?
 def resource_filename_resolver(resource_uri):
+    # TODO: add a default resolver for un-uried resources?
+    
     parsed = urlparse(resource_uri)
     if parsed.scheme not in supported_schemes:
         raise TypeError, "Scheme '%s' is unsupported" % parsed.scheme
 
     return supported_schemes[parsed.scheme](parsed)
+
+class Resource(object):
+    # TODO: check whether twisted filepath is better.
+    _resolver = staticmethod(resource_filename_resolver)
+
+    def __init__(self, uri, mode):
+        self.filename = self._resolver(uri)
+        self._mode = mode
+        self._verify_consistency()
+
+    def _verify_consistency(self):
+        # template method, should raise an exception
+        # if something is wrong.
+        pass
+
+    def stream(self):
+        return open(self.filename, self._mode)
+
+class ReadResource(Resource):
+    def __init__(self, uri, binary=False):
+        super(ReadResource, self).__init__(
+            uri, "r" + ("b" if binary else "") )
+
+    def _verify_consistency(self):
+        # TODO: rethink errors
+        # TODO: improve error reporting. If the directory has no x access, exists
+        # will just return False without better info on the problem.
+        if not os.path.exists(self.filename):
+            raise ValueError, "'%s' does not exist."
+
+
+        if not os.access(self.filename, os.R_OK):
+            raise ValueError, ("Insufficient privileges, "
+                "can't read '%s' " % self.filename)
+
+class WriteResource(Resource):
+
+    def _verify_consistency(self):
+        write_path_dir, basename = os.path.split(self.filename)
+        if not os.path.exists(write_path_dir):
+            raise ValueError, "Target write directory does not exist."
+        if not os.access(write_path_dir, os.W_OK):
+            raise ValueError, ("Insufficient privileges, "
+                "can't write '%s' in '%s' " % (basename, write_path_dir))
+        if os.path.exists(self.filename) and not os.access(self.filename, os.W_OK):
+            raise ValueError, ("Insufficient privileges, can't overwrite '%s'" %
+                self.filename)
+
+def OverwritingWriteResource(uri, binary=False):
+    return WriteResource(uri, "w" + ("b" if binary else ""))
+
+def AppendingWriteResource(uri, binary=False):
+    # TODO: do we need an appender which just appends, e.g. never creates?
+    return WriteResource(uri, "a" + ("b" if binary else ""))
+
+
+class NewFileWriteResource(WriteResource):
+    def __init__(self, uri, binary=False):
+        super(NewFileWriteResource, self).__init__(uri, "w" + ("b" if binary else ""))
+        
+    def _verify_consistency(self):
+        if os.path.exists(self.filename):
+            raise ValueError, "'%s' already exists." % self.filename
+        super(NewFileWriteResource, self)._verify_consistency()
+
+
+ 
+
+
+
+
 
 
 
--- pydenji/pydenji/test/test_resourceloader.py
+++ pydenji/pydenji/test/test_resourceloader.py
@@ -4,8 +4,15 @@
 
 from unittest import TestCase
 import os
+from tempfile import NamedTemporaryFile
+from tempfile import mkdtemp
+from shutil import rmtree
 
 from pydenji.resourceloader import resource_filename_resolver
+from pydenji.resourceloader import ReadResource
+from pydenji.resourceloader import OverwritingWriteResource
+from pydenji.resourceloader import AppendingWriteResource
+from pydenji.resourceloader import NewFileWriteResource
 
 
 
@@ -25,6 +32,124 @@ class TestPackageResolver(TestCase):
         # test just repeating what the actual implementation will be.
         # must strip Cs and Os just in case we're running from pyc or pyo.
         self.assertEquals(os.path.abspath(__file__.rstrip("co")), resolved)
+
+
+class TestResourceLoader(TestCase):
+    # think: existing == accessible, or not?
+    def setUp(self):
+        temp = NamedTemporaryFile()
+        temp.write("hello")
+        temp.flush()
+        self.temp = temp
+
+    def tearDown(self):
+        self.temp.close()
+            
+    def test_reading_loaded_resource(self):
+        # what would I like for this?
+        # - since it's a ReadResource, it should raise an exception if it does not
+        # exists or if we're unable to read it because of permission issues.
+        # - I should be able to specify additional data for the stream(), e.g. b and +
+        # the interface can be limited to stream() & filename
+
+        content = ReadResource("file://" + self.temp.name).stream().read()
+        self.assertEquals("hello", content)
+
+    def test_error_not_existing_resource(self):
+        # what would I like for this?
+        # - since it's a ReadResource, it should raise an exception if it does not
+        # exists or if we're unable to read it because of permission issues.
+        # - I should be able to specify additional data for the stream(), e.g. b and +
+        # the interface can be limited to stream() & filename
+
+        self.assertRaises(ValueError,
+            ReadResource, "file://" + "/dmfsdmfdksm/kmfdskmfksdmfksdmfdksm")
+
+    def test_error_unaccessible_resource(self):
+        os.chmod(self.temp.name, 0)
+        self.assertRaises(ValueError,
+            ReadResource, "file://" + self.temp.name)
+
+
+class TestWriteResource(TestCase):
+    def setUp(self):
+        self.tempdir = mkdtemp()
+
+    def tearDown(self):
+        rmtree(self.tempdir)
+
+    def test_if_file_exists_but_is_not_writeable_error_raised(self):
+        f = open(self.tempdir + os.sep + "newfile", "w")
+        f.close()
+        os.chmod(self.tempdir + os.sep + "newfile", 0)
+        self.assertRaises(ValueError, OverwritingWriteResource, "file://" + self.tempdir + os.sep + "newfile")
+        
+    def test_overwriting_resource_allows_writing_if_file_does_not_exist(self):
+        resource = OverwritingWriteResource("file://" + self.tempdir + os.sep + "newfile")
+
+    def test_overwriting_resource_allows_writing_if_file_exists(self):
+        f = open(self.tempdir + os.sep + "newfile", "w")
+        f.write("abc")
+        f.flush()
+        resource = OverwritingWriteResource("file://" + self.tempdir + os.sep + "newfile")
+
+    def test_overwriting_allows_writing_to_resource(self):
+        resource = OverwritingWriteResource("file://" + self.tempdir + os.sep + "newfile")
+        stream = resource.stream()
+        stream.write("abc")
+        stream.close()
+
+        f = open(self.tempdir + os.sep + "newfile")
+        self.assertEquals("abc", f.read())
+
+    def test_appending_writing_resource_appends_if_file_exists(self):
+        f = open(self.tempdir + os.sep + "newfile", "w")
+        f.write("asd")
+        f.close()
+
+        resource = AppendingWriteResource("file://" + self.tempdir + os.sep + "newfile")
+        stream = resource.stream()
+        stream.write("fgh")
+        stream.close()
+        
+        f = open(self.tempdir + os.sep + "newfile", "r")
+        self.assertEquals("asdfgh", f.read())
+
+    def test_appending_writing_resource_creates_if_file_does_not_exist(self):
+        resource = AppendingWriteResource("file://" + self.tempdir + os.sep + "newfile")
+        stream = resource.stream()
+        stream.write("fgh")
+        stream.close()
+
+        f = open(self.tempdir + os.sep + "newfile", "r")
+        self.assertEquals("fgh", f.read())
+
+    def test_newfile_write_resource_creates_if_file_does_not_exist(self):
+        resource = NewFileWriteResource("file://" + self.tempdir + os.sep + "newfile")
+
+    def test_newfile_write_resource_raises_error_if_file_exists(self):
+        f = open(self.tempdir + os.sep + "newfile", "w")
+        f.write("asd")
+        f.close()
+
+        self.assertRaises(ValueError, NewFileWriteResource, "file://" + self.tempdir + os.sep + "newfile")
+
+
+#
+    def disabletest_maybe_resource(self):
+        # sometimes we're uncertain whether a certain read resource exists
+        # and we don't want to throw an exception if it doesn't.
+        # the interface is slightly different and it's clear that it's a factory
+
+        # should we call this firstExistingResource?
+        maybe, resource = maybeReadResource("gianni", "pinotto", "giannina",
+                                        additional_mode="...")
+        # return the first existing resource between those that were passed.
+        # if at least one exists, maybe is true and resource is a ReadResource;
+        # if all resources get checked and none exists, maybe is False and resource
+        # is None
+
+
 
 
 
