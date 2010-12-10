61ccd679bf24153acce96d3e9557e8e4bef3d34b
Alan Franzoni <username@franzoni.eu>
2010-12-10 17:10:47 +0100

    Enh: arg wiring support for default args.

--- pydenji/pydenji/config/argwiring.py
+++ pydenji/pydenji/config/argwiring.py
@@ -5,18 +5,36 @@
 # fetches the required args and kwargs for a function from a mapping.
 
 from inspect import getargspec
+from pydenji.lib.odict import OrderedDict
 
 _NO_VALUE = object()
+
 
 def wire(callable_obj, mapping, *additional_args, **additional_kwargs):
     args, varargs, varkw, defaults = getargspec(callable_obj)
 
     # let's determine which args can be fetched from the dict:
-    call_args = []
-    for arg in args:
-        call_args.append(mapping.get(arg, _NO_VALUE))
+    current_arguments = OrderedDict()
 
-    return callable_obj(*call_args)
+    for arg in args:
+        current_arguments[arg] = mapping.get(arg, _NO_VALUE)
+
+    # complete missing args with default args
+    for default_value, (arg_name, current_value) in zip(reversed(defaults),
+                reversed(current_arguments.items())):
+        if current_value is _NO_VALUE:
+            current_arguments[arg_name] = default_value
+
+    # if there's still any _NO_VALUE, we're in trouble. let's collect
+    # which keys are missing in order to convey a good error message.
+    missing_keys = "','".join([key for key, value in current_arguments.iteritems()
+        if value is _NO_VALUE])
+    if missing_keys:
+        raise TypeError, ("Could not wire '%s':\n"
+            "Can't determine values for the following args: '%s'\n" % (
+                callable_obj, missing_keys))
+
+    return callable_obj(*current_arguments.values())
 
 
 
--- pydenji/pydenji/test/test_argwiring.py
+++ pydenji/pydenji/test/test_argwiring.py
@@ -4,7 +4,7 @@
 
 import unittest
 
-from pydenji.config.argwiring import wire
+from pydenji.config.argwiring import wire, _NO_VALUE
 
 def func_to_wire(pos1, pos2, kw1="a", kw2="b"):
     return (pos1, pos2, kw1, kw2)
@@ -14,6 +14,18 @@ class  TestArgwiring(unittest.TestCase):
         d = { "pos1":1, "pos2": 2, "kw1": 3, "kw2": 4 }
         obj = wire(func_to_wire, d)
         self.assertEquals( (1,2,3,4), obj )
+
+    def test_missing_args_from_mapping_are_fetched_from_defaults(self):
+        d = { "pos1":1, "pos2": 2, "kw1": 3 }
+        obj = wire(func_to_wire, d)
+        self.assertEquals( (1,2,3,"b"), obj )
+
+    def test_error_raised_if_required_args_cant_be_completed(self):
+        d = { "pos1":1, }
+        self.assertRaises(TypeError, wire, func_to_wire, d )
+
+      
+
 
 
 if __name__ == '__main__':
