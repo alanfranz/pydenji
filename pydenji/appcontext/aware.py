dbab045cdf6b7c688d216fc74cd7e61c1cdff474
Alan Franzoni <username@franzoni.eu>
2011-05-21 11:34:34 +0200

    Enh: support ABC, ducktyped appcontextaware interface.

--- pydenji/appcontext/aware.py
+++ pydenji/appcontext/aware.py
@@ -2,8 +2,20 @@
 # -*- coding: utf-8 -*-
 # (C) 2010 Alan Franzoni
 
+from abc import abstractmethod
+from ducktypes.ducktype import DuckABCMeta
+
+
 def is_appcontext_aware(obj):
     # TODO: should we check for callability as well?
     if getattr(obj, "set_app_context", None) is not None:
         return True
     return False
+
+class AppContextAware(object):
+    __metaclass__ = DuckABCMeta
+
+    @abstractmethod
+    def set_app_context(self, context):
+        pass
+
