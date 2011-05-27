def5593be2abd2f186bd8d6ebf97e8acdcf20316
Alan Franzoni <username@franzoni.eu>
2011-05-28 00:33:28 +0200

    Refactoring start; simpler appcontext interface.

--- pydenji/appcontext/context.py
+++ pydenji/appcontext/context.py
@@ -7,53 +7,69 @@ from pydenji.appcontext.aware import AppContextAware
 from pydenji.config.composite import CompositeConfig
 
 
-class UnconfiguredError(Exception):
+class UnknownProviderException(Exception):
     pass
 
+
 class AppContext(object):
-    def __init__(self, *configurations):
-        conf = CompositeConfig(configurations)
-        self._names_factories = self._get_all_factories(conf)
-        conf.set_app_context(self)
-        self._start(self._names_factories)
+    def __init__(self):
+        self._names_providers = {}
+#        pass
+
+#                 *configurations):
+#        self._names_factories = {}
+#        for conf in configurations:
+#            self.register(conf.__name__, conf)
+        #self._start(self._names_factories)
+
+    def register(self, name, provider):
+        # TODO: what to do if there's already a provider for that name?
+        self._names_providers[name] = provider
+        # this is something we do for configs, makes no sense for other
+        # object, we'd probably MUCH better create an interface for configuration
+        # objects.
+        #names_factories = self._get_all_factories(provider)
+        # TODO: this does not check for overwrites, should it?
+        #self._names_factories.update(names_factories)
 
     def __contains__(self, key):
-        return key in self._names_factories
+        return key in self._names_providers
 
     def __iter__(self):
-        return self._names_factories.iterkeys()
+        return self._names_providers.iterkeys()
 
 
     def provide(self, name, *args, **kwargs):
         try:
-            factory = self._names_factories[name]
+            provider = self._names_providers[name]
         except KeyError:
-            raise UnconfiguredError, "No factory was configured for '%s'" % name
-        return self._get_instance(factory, *args, **kwargs)
+            raise UnknownProviderException, "No provider was configured for '%s'" % name
+        return self._get_instance(provider, *args, **kwargs)
     
-    def _start(self, names_factories):
-        for factory in names_factories.values():
-            if is_eager(factory):
-                self._get_instance(factory)
+#    def _start(self, names_factories):
+#        for factory in names_factories.values():
+#            if is_eager(factory):
+#                self._get_instance(factory)
 
     def _get_instance(self, factory, *args, **kwargs):
         # this way the set_app_context() method will be called multiple times,
         # even though the object is a singleton. While it should make no harm,
         # we should think about it, might it do any harm?
-        obj = factory(*args, **kwargs)
-        if isinstance(obj, AppContextAware):
-            obj.set_app_context(self)
-        return obj
+        return factory(*args, **kwargs)
 
-    @staticmethod
-    def _get_all_factories(config):
-        # TODO: refactor using a list comprehension/filter expr.
-        names_factories = {}
-        for attr in dir(config):
-            value = getattr(config, attr)
-            if is_object_factory(value):
-                names_factories[attr] = value
-        return names_factories
+#        if isinstance(obj, AppContextAware):
+#            obj.set_app_context(self)
+        #return obj
+
+#    @staticmethod
+#    def _get_all_factories(config):
+#        # TODO: refactor using a list comprehension/filter expr.
+#        names_factories = {}
+#        for attr in dir(config):
+#            value = getattr(config, attr)
+#            if is_object_factory(value):
+#                names_factories[attr] = value
+#        return names_factories
 
 
 
--- pydenji/test/test_appcontext.py
+++ pydenji/test/test_appcontext.py
@@ -4,152 +4,168 @@
 
 from unittest import TestCase
 
-from pydenji.appcontext.context import AppContext
+from pydenji.appcontext.context import AppContext, UnknownProviderException
 from pydenji.appcontext.aware import AppContextAware
 from pydenji.config.contextconfig import ContextConfiguration
 from pydenji.config.pythonconfig import singleton, dontconfigure
 from pydenji.config.pythonconfig import Configuration
 
-class TestAppContextAwareness(TestCase):
-    def test_objects_offering_set_app_context_are_appcontext_aware(self):
-        class SomeObj(object):
-            def set_app_context(self, context):
-                pass
-
-        self.assertTrue(isinstance(SomeObj(), AppContextAware))
-
-    def test_objects_not_offering_set_app_context_are_not_appcontext_aware(self):
-        class SomeObj(object):
-            pass
-
-        self.assertFalse(isinstance(SomeObj(), AppContextAware))
-
 
 class TestAppContext(TestCase):
-    def test_appcontext_allows_retrieving_by_name(self):
+    def setUp(self):
+        self.appcontext = AppContext()
+        self.appcontext.register("myname", lambda x,y : x*y)
 
-        class MockConf(object):
-            @singleton
-            def something(self):
-                return 1
-        MockConf = Configuration(MockConf)
-        
-        context = AppContext(MockConf())
-        something = context.provide("something")
-        self.assertEquals(1, something)
+    def test_provide_creates_object_from_registered_provider(self):
+        self.assertEquals(6, self.appcontext.provide("myname", 3, 2))
 
-    def test_appcontext_supports_multiple_configs(self):
-        # TODO: this functionality might be overlapping to CompositeConfig.
-        # think about it.
-        
-        
-        class MockConf(object):
-            @singleton
-            def something(self):
-                return 1
-        MockConf = Configuration(MockConf)
+    def test_provide_raises_exception_if_unknown_name(self):
+        self.assertRaises(UnknownProviderException, self.appcontext.provide, "noname")
 
-        class OtherConf(object):
-            @singleton
-            def otherthing(self):
-                return 2
-
-        OtherConf = Configuration(OtherConf)
-
-        context = AppContext(MockConf(), OtherConf())
-        something = context.provide("something")
-        self.assertEquals(1, something)
-        otherthing = context.provide("otherthing")
-        self.assertEquals(2, otherthing)
-
-
-
-    def test_appcontext_fetches_objects_eagerly_when_required(self):
-        c = set()
-
-        class MockConf(object):
-            @singleton
-            def something(self):
-                c.add("something")
-
-            @singleton
-            def _private(self):
-                c.add("private")
-
-        MockConf = Configuration(MockConf)
-             
-
-        conf = MockConf()
-        context = AppContext(conf)
-        self.assertEquals(set(["something", "private"]), c)
-
-        
-    def test_appcontext_fetches_objects_lazily_when_required(self):
-        c = []
-
-        class MockConf(object):
-            @singleton.lazy
-            def something(self):
-                c.append(True)
-        MockConf = Configuration(MockConf)
-
-
-        conf = MockConf()
-        context = AppContext(conf)
-        self.assertEquals([], c)
-
-    def test_appcontext_gets_injected_on_aware_objects(self):
-        # TODO: think whether we need to use an ABC instead or as well.
-        class AppAwareObject(object):
-            app_context = None
-            
-            def set_app_context(self, context):
-                self.app_context = context
-
-
-        class MockConf(object):
-            @singleton
-            def appcontextaware(self):
-                return AppAwareObject()
-        MockConf = Configuration(MockConf)
-
-        context = AppContext(MockConf())
-        aware = context.provide("appcontextaware")
-        self.assertTrue(context is aware.app_context, "context wasn't injected correctly!")
-
-    def test_appcontext_gets_injected_on_aware_configuration_objects(self):
-
-        class MockConf(object):
-            @dontconfigure
-            def set_app_context(self, context):
-                self.app_context = context
-        
-        MockConf = Configuration(MockConf)
-
-        conf = MockConf()
-        context = AppContext(conf)
-        self.assertTrue(context is conf.app_context, "context wasn't injected correctly!")
-
-
-class TestGlobalConfig(TestCase):
-    def test_global_config_falls_back_on_appcontext_factories(self):
-        
-        class One(object):
-            def relies_on_other(self):
-                return self.other() * 2
-            
-        class Other(object):
-            def other(self):
-                return 2
-
-        one = ContextConfiguration(One)()
-        other = Configuration(Other)()
-
-        context = AppContext(one, other)
-        self.assertEquals(4, context.provide("relies_on_other"))
 
 
     
 
-
-
+#class TestAppContextAwareness(TestCase):
+#    def test_objects_offering_set_app_context_are_appcontext_aware(self):
+#        class SomeObj(object):
+#            def set_app_context(self, context):
+#                pass
+#
+#        self.assertTrue(isinstance(SomeObj(), AppContextAware))
+#
+#    def test_objects_not_offering_set_app_context_are_not_appcontext_aware(self):
+#        class SomeObj(object):
+#            pass
+#
+#        self.assertFalse(isinstance(SomeObj(), AppContextAware))
+#
+#
+#class TestAppContext(TestCase):
+#    def test_appcontext_allows_retrieving_by_name(self):
+#
+#        class MockConf(object):
+#            @singleton
+#            def something(self):
+#                return 1
+#        MockConf = Configuration(MockConf)
+#
+#        context = AppContext(MockConf)
+#        something = context.provide("something")
+#        self.assertEquals(1, something)
+#
+#    def test_appcontext_supports_multiple_configs(self):
+#        # TODO: this functionality might be overlapping to CompositeConfig.
+#        # think about it.
+#
+#
+#        class MockConf(object):
+#            @singleton
+#            def something(self):
+#                return 1
+#        MockConf = Configuration(MockConf)
+#
+#        class OtherConf(object):
+#            @singleton
+#            def otherthing(self):
+#                return 2
+#
+#        OtherConf = Configuration(OtherConf)
+#
+#        context = AppContext(MockConf, OtherConf)
+#        something = context.provide("something")
+#        self.assertEquals(1, something)
+#        otherthing = context.provide("otherthing")
+#        self.assertEquals(2, otherthing)
+#
+#
+#
+#    def test_appcontext_fetches_objects_eagerly_when_required(self):
+#        c = set()
+#
+#        class MockConf(object):
+#            @singleton
+#            def something(self):
+#                c.add("something")
+#
+#            @singleton
+#            def _private(self):
+#                c.add("private")
+#
+#        MockConf = Configuration(MockConf)
+#
+#
+#        conf = MockConf
+#        context = AppContext(conf)
+#        self.assertEquals(set(["something", "private"]), c)
+#
+#
+#    def test_appcontext_fetches_objects_lazily_when_required(self):
+#        c = []
+#
+#        class MockConf(object):
+#            @singleton.lazy
+#            def something(self):
+#                c.append(True)
+#        MockConf = Configuration(MockConf)
+#
+#
+#        conf = MockConf
+#        context = AppContext(conf)
+#        self.assertEquals([], c)
+#
+#    def test_appcontext_gets_injected_on_aware_objects(self):
+#        # TODO: think whether we need to use an ABC instead or as well.
+#        class AppAwareObject(object):
+#            app_context = None
+#
+#            def set_app_context(self, context):
+#                self.app_context = context
+#
+#
+#        class MockConf(object):
+#            @singleton
+#            def appcontextaware(self):
+#                return AppAwareObject()
+#        MockConf = Configuration(MockConf)
+#
+#        context = AppContext(MockConf)
+#        aware = context.provide("appcontextaware")
+#        self.assertTrue(context is aware.app_context, "context wasn't injected correctly!")
+#
+#    def test_appcontext_gets_injected_on_aware_configuration_objects(self):
+#
+#        class MockConf(object):
+#            @dontconfigure
+#            def set_app_context(self, context):
+#                self.app_context = context
+#
+#        MockConf = Configuration(MockConf)
+#
+#        conf = MockConf
+#        context = AppContext(conf)
+#        self.assertTrue(context is conf.app_context, "context wasn't injected correctly!")
+#
+#
+#class TestGlobalConfig(TestCase):
+#    def test_global_config_falls_back_on_appcontext_factories(self):
+#
+#        class One(object):
+#            def relies_on_other(self):
+#                return self.other() * 2
+#
+#        class Other(object):
+#            def other(self):
+#                return 2
+#
+#        one = ContextConfiguration(One)
+#        other = Configuration(Other)
+#
+#        context = AppContext(one, other)
+#        self.assertEquals(4, context.provide("relies_on_other"))
+#
+#
+#
+#
+#
+#
