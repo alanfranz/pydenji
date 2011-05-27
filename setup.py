7003608bd799e6352f31f9272ecfff24f8999c03
Alan Franzoni <username@franzoni.eu>
2011-05-27 11:41:02 +0200

    Fix: dev is already set in setup.cfg.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ if sys.version_info[1] < 7:
 setup(name='pydenji',
       description='Dependency Injection Toolkit',
       # add byteplay as soon as it works.
-      version='0.6.0dev',
+      version='0.6.0',
       install_requires=requirements,
       long_description=open("README").read(),
       author='Alan Franzoni',
