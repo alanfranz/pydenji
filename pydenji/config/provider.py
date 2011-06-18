b806ea0357b748df83e2de59db22c864645ff74e
Alan Franzoni <username@franzoni.eu>
2011-06-18 18:22:13 +0200

    Ref: extract annotation-based singleton/eager function.

--- pydenji/config/provider.py
+++ pydenji/config/provider.py
@@ -42,11 +42,15 @@ def singleton(func, eager=True):
             maybevalue.has_value = True
         return maybevalue.value
 
-    setattr(singleton_wrapped, _INSTANTIATE_EAGERLY, eager)
-    setattr(singleton_wrapped, _CONFIGURED_OBJECT_FACTORY, True)
-    setattr(singleton_wrapped, _SHOULD_CONFIGURE, False)
+    set_singleton(singleton_wrapped, eager)
 
     return singleton_wrapped
+
+def set_singleton(obj, eager):
+    setattr(obj, _INSTANTIATE_EAGERLY, eager)
+    setattr(obj, _CONFIGURED_OBJECT_FACTORY, True)
+    setattr(obj, _SHOULD_CONFIGURE, False)
+
 
 singleton.lazy = partial(singleton, eager=False)
 singleton.default_lazy_init = False
